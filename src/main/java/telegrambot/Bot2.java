67454d78c4b6173b327948af1a87ccdc58cc34f1
Philipp983 <Philipp-Wagner91@web.de>
2024-05-26 19:57:50 +0200

    created "workable" situation, where voice recording is saved as ogg file, but the api for translation into text is not implemented

--- src/main/java/telegrambot/Bot2.java
+++ src/main/java/telegrambot/Bot2.java
@@ -1,7 +1,11 @@
 package telegrambot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.objects.File;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import telegrambot.apiclients.speechtotext.App;
 import telegrambot.apiclients.texttospeech.TextToSpeechAPI;
 import telegrambot.catapiclient.CatApiClient;
 import telegrambot.configuration.CommandRegistry;
@@ -10,6 +14,10 @@ import telegrambot.quiz.Millionaire;
 import telegrambot.telegram_ui.TelegramMenuUi;
 import telegrambot.apiclients.weatherapiclient.WeatherApiClient;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -74,6 +82,31 @@ public class Bot2 extends TelegramLongPollingBot {
 				isWeatherGenerated = false;
 			}
 			id = update.getCallbackQuery().getMessage().getChatId();
+		} 			else if (update.hasMessage() && update.getMessage().hasVoice()) {
+			id = update.getMessage().getChatId();
+			System.out.println("Has voice");
+			System.out.println("is this true: " + update.getMessage().hasText());
+			System.out.println(update.getMessage().getVoice());
+			String fileId = update.getMessage().getVoice().getFileId();
+			try {
+				downloadVoiceFile(fileId, id);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try{
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			txt = String.valueOf(App.transcribeAudioToText(Config.getProperty("voice_recording.path") + "voiceRecording.ogg"));
+
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(txt);
 		}
 
 		if ((update.hasMessage() || update.hasCallbackQuery()) && !isContaced) {
@@ -155,6 +188,46 @@ public class Bot2 extends TelegramLongPollingBot {
 		if (command != null) {
 			command.execute(id, input, this);
 		}
+	}
+
+		private void downloadVoiceFile(String fileId, long chatId) throws IOException {
+		// Get the file path from Telegram API
+		GetFile getFileRequest = new GetFile();
+		getFileRequest.setFileId(fileId);
+
+		File file = null;
+		try {
+			file = execute(getFileRequest);
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Retrieve the base file path from the properties file
+		String baseFilePath = Config.getProperty("voice_recording.path");
+		System.out.println("Base file path: " + baseFilePath);
+
+		// Ensure the directory exists
+		java.io.File directory = new java.io.File(baseFilePath).getParentFile();
+		if (!directory.exists()) {
+			directory.mkdirs();
+		}
+
+		// Construct the full file path with the file ID
+		String localFilePath = baseFilePath + "voiceRecording.ogg";
+		java.io.File localFile = new java.io.File(localFilePath);
+
+		// Construct the URL to download the file
+		String fileUrl = "https://api.telegram.org/file/bot" + getBotToken() + "/" + file.getFilePath();
+		System.out.println("File URL: " + fileUrl);
+
+		// Download the file from the URL
+		try (InputStream in = new URL(fileUrl).openStream()) {
+			Files.copy(in, localFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		}
+
+		System.out.println("Voice file downloaded to: " + localFile.getAbsolutePath());
+
+		// Now you can process the voice file as needed
 	}
 
 	public void activateFunctionality(String functionality) {
